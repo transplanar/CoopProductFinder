3dac37cfa806678032695f2d7a0bd8597ea55acc
transplanar <transplanar@gmail.com>
2017-01-03 21:53:18 -0500

    Code cleanup and merge

--- app/models/product.rb
+++ app/models/product.rb
@@ -125,55 +125,23 @@ class Product < ActiveRecord::Base
     new_str.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
   end
 
-  # FIXME prevent from finding things out of order
   def self.format_term string, sub_string
-    puts "FORMATTING STRING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    puts "FORMATTING STRING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-
-
-    # string_arr = string.split(//)
-    string_arr = string.chars
-    sub_string = sub_string.gsub('%','')
-
-    # puts "reverse string #{string.reverse}"
-    # puts "reverse string #{sub_string.reverse}"
-
-    #
-    # Test non-consective
-    # sub_string.chars.reverse_each do |char|
-    #   test = string.upto(char){|e| e}
-    #   puts "test #{test}"
-    #   if string.index(test)
-    #     puts "#{test} found at #{string.index(test)}"
-    #   end
-    # end
-
     full_string = []
-    current_string_arr = string_arr
+    sub_string = sub_string.gsub('%','')
+    string_arr = string.chars
 
     sub_string.split(//).each do |char|
-      # index = string_arr.find_index{|e| e.upcase == char || e.downcase == char}
-      index = current_string_arr.find_index{|e| e.upcase == char || e.downcase == char}
+      index = string_arr.find_index{|e| e.upcase == char || e.downcase == char}
 
-      # formatted_term = "<span class='matched_letter_highlight'>#{string_arr[index]}</span>"
-      formatted_term = "<span class='matched_letter_highlight'>#{current_string_arr[index]}</span>"
-      # string_arr[index] = formatted_term
-      current_string_arr[index] = formatted_term
-      full_string << current_string_arr[0..index]
-      # current_string_arr = string_arr[index..string_arr.length]
-      puts "Current string BEFORE #{current_string_arr}"
-      current_string_arr = current_string_arr[index+1..string_arr.length]
-      puts "Current string AFTER #{current_string_arr}"
-      puts "Full string #{full_string}"
-      puts "********************************************"
+      formatted_term = "<span class='matched_letter_highlight'>#{string_arr[index]}</span>"
+      string_arr[index] = formatted_term
+      full_string << string_arr[0..index]
+      string_arr = string_arr[index+1..string_arr.length]
     end
 
-    full_string << current_string_arr
+    full_string << string_arr
 
-    # new_string = string_arr.join
-    # new_string = current_string_arr.join
     new_string = full_string.join
-    puts "FULL STRING #{new_string}"
 
     return new_string
   end
